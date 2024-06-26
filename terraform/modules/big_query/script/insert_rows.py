33d0e459e95ac279685593b5fbe59c8f48b6c875
Vibesancell <mark.polubotko@gmail.com>
2024-06-27 00:40:52 +0300

    update script

--- terraform/modules/big_query/script/insert_rows.py
+++ terraform/modules/big_query/script/insert_rows.py
@@ -2,22 +2,34 @@ import os
 import random
 import string
 from google.cloud import bigquery
-from google.auth import compute_engine
+from google.auth import default, exceptions
 
-credentials = compute_engine.Credentials()
+def get_credentials():
+    try:
+        credentials, _ = default()
+        return credentials
+    except exceptions.DefaultCredentialsError as e:
+        print(f"Failed to obtain credentials: {e}")
+        raise
 
-client = bigquery.Client(credentials=credentials, project="tt-devops-427513")
+def main():
+    credentials = get_credentials()
 
-dataset_id = "prod_tt_devops_dataset"
-table_id = "prod_tt_devops_table"
+    client = bigquery.Client(credentials=credentials, project="your-project-id")
 
-rows_to_insert = [
-    {"Id": i, "Tag": random.choice(string.ascii_uppercase), "Count": random.randint(0, 9)}
-    for i in range(1000)
-]
+    dataset_id = "example_dataset"
+    table_id = "example_table"
 
-errors = client.insert_rows_json(f"{dataset_id}.{table_id}", rows_to_insert)
-if errors:
-    print(f"Errors occurred while inserting rows: {errors}")
-else:
-    print("Rows successfully inserted.")
+    rows_to_insert = [
+        {"Id": i, "Tag": random.choice(string.ascii_uppercase), "Count": random.randint(0, 9)}
+        for i in range(1000)
+    ]
+
+    errors = client.insert_rows_json(f"{dataset_id}.{table_id}", rows_to_insert)
+    if errors:
+        print(f"Errors occurred while inserting rows: {errors}")
+    else:
+        print("Rows successfully inserted.")
+
+if __name__ == "__main__":
+    main()
